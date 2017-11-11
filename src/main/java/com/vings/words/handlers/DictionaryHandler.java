8405a918fb29fe106c8832838f4a9409461d0805
Andrew Pryshchepa <vinglfm@gmail.com>
2017-11-12 01:10:25 +0200

    Refactoring save word method

--- src/main/java/com/vings/words/handlers/DictionaryHandler.java
+++ src/main/java/com/vings/words/handlers/DictionaryHandler.java
@@ -3,14 +3,12 @@ package com.vings.words.handlers;
 import com.vings.words.model.Word;
 import com.vings.words.repository.WordsRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-import static org.springframework.web.reactive.function.BodyExtractors.toFormData;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
@@ -31,9 +29,9 @@ public class DictionaryHandler {
     }
 
     public Mono<ServerResponse> save(ServerRequest serverRequest) {
-        return serverRequest.body(toFormData())
-                .flatMap(formData -> ok().body(wordsRepository.save(new Word("word", "translation")), Word.class))
-                .switchIfEmpty(ServerResponse.badRequest()
-                        .build());
+        return serverRequest.bodyToMono(Word.class)
+                .filter(word -> word.getWord() != null && word.getTranslation() != null)
+                .flatMap(word -> ok().body(wordsRepository.save(word), Word.class))
+                .switchIfEmpty(ServerResponse.badRequest().build());
     }
 }
