ef705cb4e4ad3c4616efa19b898a7ed6a509b2a1
Andrew Pryshchepa <vinglfm@gmail.com>
2017-12-31 10:40:17 +0200

    Added test cases for quiz servlet API

--- src/test/java/com/vings/words/servlet/QuizServletTest.java
+++ src/test/java/com/vings/words/servlet/QuizServletTest.java
@@ -3,6 +3,7 @@ package com.vings.words.servlet;
 import com.datastax.driver.core.utils.UUIDs;
 import com.vings.words.WordsApplication;
 import com.vings.words.model.Word;
+import com.vings.words.model.Word.WordBuilder;
 import com.vings.words.model.quiz.Sprint;
 import com.vings.words.repository.WordsRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -15,10 +16,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -38,10 +36,11 @@ class QuizServletTest {
     private String user = "user1";
     private UUID category1 = UUIDs.random();
     private UUID category2 = UUIDs.random();
-    private List<Word> words = Arrays.asList(new Word.WordBuilder(user, category1, "Reactive").withAnswers(16).withTranslation(new HashSet<>(singletonList("Реактив"))).build(),
-            new Word.WordBuilder(user, category1, "Core").withAnswers(100).withTranslation(new HashSet<>(singletonList("Основа"))).build(),
-            new Word.WordBuilder(user, category1, "Tangos").withAnswers(95).withTranslation(new HashSet<>(asList("Тангос"))).build(),
-            new Word.WordBuilder(user, category2, "Tango").withAnswers(95).withTranslation(new HashSet<>(asList("Танго", "Супер"))).build());
+    private List<Word> words = Arrays.asList(new WordBuilder(user, category1, "Reactive").withAnswers(16).withTranslation(new HashSet<>(singletonList("Реактив"))).build(),
+            new WordBuilder(user, category1, "Reactor").withAnswers(8).withTranslation(new HashSet<>(singletonList("Реактор"))).build(),
+            new WordBuilder(user, category1, "Core").withAnswers(100).withTranslation(new HashSet<>(singletonList("Основа"))).build(),
+            new WordBuilder(user, category1, "Tangos").withAnswers(95).withTranslation(new HashSet<>(singletonList("Тангос"))).build(),
+            new WordBuilder(user, category2, "Tango").withAnswers(95).withTranslation(new HashSet<>(asList("Танго", "Супер"))).build());
 
     @BeforeEach
     void setUp() {
@@ -57,14 +56,50 @@ class QuizServletTest {
     }
 
     @Test
-    void getSpringQuestions() {
+    void getSprintQuestions() {
         wordsRepository.saveAll(words).blockLast();
 
         List<Sprint> sprints = client.get().uri("/quiz/sprint/{user}/{category}/{page}/{offset}", user, category1, 0, 10).exchange()
                 .expectStatus().isOk()
                 .expectBodyList(Sprint.class).returnResult().getResponseBody();
 
-        assertThat(sprints).hasSize(2);
-        assertThat(sprints).extracting("word").containsExactly("Reactive", "Tangos");
+        assertThat(sprints).hasSize(3);
+        assertThat(sprints).extracting("word").containsExactly("Reactive", "Reactor", "Tangos");
+        assertThat(sprints).extracting("answer").doesNotContainNull();
     }
+
+    @Test
+    void getSprintQuestionsWithOffset() {
+        wordsRepository.saveAll(words).blockLast();
+
+        List<Sprint> sprints = client.get().uri("/quiz/sprint/{user}/{category}/{page}/{offset}", user, category1, 1, 2).exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Sprint.class).returnResult().getResponseBody();
+
+        assertThat(sprints).hasSize(1);
+        assertThat(sprints).extracting("word").containsExactly("Tangos");
+        assertThat(sprints).extracting("answer").doesNotContainNull();
+    }
+
+    @Test
+    void badRequestWhenGetSprintQuestionsWithInvalidCategoryUUID() {
+        wordsRepository.saveAll(words).blockLast();
+
+        String notValidCategoryUUID = "notValidCategoryUUID";
+        client.get().uri("/quiz/sprint/{user}/{category}/{page}/{offset}", user, notValidCategoryUUID, 1, 2).exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void getSprintQuestionsWhenUserDoesNotExist() {
+        wordsRepository.saveAll(words).blockLast();
+
+        String notExistingUser = "notExistingUser";
+        List<Sprint> sprints = client.get().uri("/quiz/sprint/{user}/{category}/{page}/{offset}", notExistingUser, category1, 1, 2).exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Sprint.class).returnResult().getResponseBody();
+
+        assertThat(sprints).isEmpty();
+    }
+
 }
