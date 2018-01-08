75ff99ec9052b44fdeb44efaf44d077213b0c7f5
Andrew Pryshchepa <vinglfm@gmail.com>
2018-01-08 17:59:44 +0200

    Added database changes to support examples for word

--- src/main/java/com/vings/words/model/Example.java
+++ src/main/java/com/vings/words/model/Example.java
@@ -2,11 +2,13 @@ package com.vings.words.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.cassandra.core.mapping.UserDefinedType;
 
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@UserDefinedType
 public class Example {
 
     private String word;
